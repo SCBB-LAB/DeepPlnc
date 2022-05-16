15017b2ddf39971fd87ec7ea67ef1c86aad2ee95
Studio of Computational Biology and Bioinformatics <85485516+SCBB-LAB@users.noreply.github.com>
2022-05-16 12:37:59 +0530

    Delete batch-plot.py

--- batch-plot.py
+++
@@ -1,8 +0,0 @@
-import os, sys
-import pandas as pd
-import plotly.express as px
-
-
-df = pd.read_csv(str(sys.argv[1])+'.csv')
-fig = px.violin(df, x="<b>Sequence</b>", y="<b>Score</b>", color="<b>Sequence</b>", box=True,title="<b>Violin plot depicting score distribution on ten different sequence</b>")
-fig.show()
